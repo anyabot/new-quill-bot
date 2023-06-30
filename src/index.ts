3ba1f777d2cfaed435e60ca157d766bca73297ed
anyabot <52899201+anyabot@users.noreply.github.com>
2023-06-30 13:58:41 +0900

    nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
-import dotenv from 'dotenv'
-dotenv.config()
 
 const client = new SapphireClient({
   intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions],
